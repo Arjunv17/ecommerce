659908932119503a4dd51b205757bf0c584414fc
9814587362 <prabhjotkaur@codebrewinnovations.com>
2022-06-15 15:15:18 +0530

    seconf commit

--- server/handler/handler.js
+++ server/handler/handler.js
@@ -1,25 +1,9 @@
-const mongoose = require('mongoose');
-const handler =  require('../v1/controllers/index')
+// const mongoose = require('mongoose');
+// const handler =  require('../v1/controllers/index')
 
-module.exports.userRegister = async (userData)=>{
-    return new Promise((resolve, reject)=>{
-        request({
-            method: 'POST',
-            // url: constants.apis.continuevideos,
-            body: userData,
-            headers: {
-              "Content-Type": "application/json",
-            //   "Authorization": 'Bearer ' + token
-            },
-            json: true,
-          }, (err, successData) => {
-            if (err) {
-              reject(err)
-            } else {
-              resolve(successData)
-              console.log("Success")
-            }
-          })
-    })
-}
+// module.exports.userRegister = async (userData)=>{
+//     return new Promise((resolve, reject)=>{
+        
+//     })
+// }
 
